bc71316d9d046b18b4bbdc413f03bec204092a73
rbustami <rbustam@us.ibm.com>
2019-09-29 19:29:36 +0200

    Added private_network_only flag to private vsi

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -47,4 +47,5 @@ resource "ibm_compute_vm_instance" "compute_instances2" {
   private_security_group_ids = ["${ibm_security_group.sg2.id}"]
   local_disk = false
   private_vlan_id = "${var.privatevlanid}"
+  private_network_only = true
 }
